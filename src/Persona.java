cacd2c6d853ede8a46f371e2f0a0fda5c9460cc8
Vinici0 <75345399+Vinici0@users.noreply.github.com>
2021-02-27 11:26:01 -0500

    Primer avance

---
+++ src/Persona.java
@@ -0,0 +1,23 @@
+
+public class Persona {
+	
+	private String nombre;
+	private String apellido;
+	private String domicilio;
+	private int edad;
+	
+	public Persona(String nombre, String apellido, String domicilio, int edad) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.domicilio = domicilio;
+		this.edad = edad;
+	}
+	
+	
+	
+	
+	
+	
+
+}
