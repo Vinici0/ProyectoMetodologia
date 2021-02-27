2a4abb9a91b670dbb7099e152d4fbeabfe747ff8
Vinici0 <75345399+Vinici0@users.noreply.github.com>
2021-02-27 11:35:07 -0500

    Avance 2

--- src/Persona.java
+++ src/Persona.java
@@ -13,6 +13,38 @@ public class Persona {
 		this.domicilio = domicilio;
 		this.edad = edad;
 	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getDomicilio() {
+		return domicilio;
+	}
+
+	public void setDomicilio(String domicilio) {
+		this.domicilio = domicilio;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
 	
 	
 	
